05b0f461a78d6ecc83081ace57f2fa769341f63d
Piyush Patel <piyushpatel2005@gmail.com>
2020-10-24 19:40:22 -0400

    singleton design pattern

---
+++ src/main/java/singleton/NonThreadSafeSingleton.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public class NonThreadSafeSingleton {
+
+    private static NonThreadSafeSingleton instance = null;
+
+    private NonThreadSafeSingleton() {}
+
+    public static NonThreadSafeSingleton getInstance() {
+        if(instance == null) {
+            instance = new NonThreadSafeSingleton();
+        }
+
+        return instance;
+    }
+}
