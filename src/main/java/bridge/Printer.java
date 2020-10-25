c0a6cb34cdf37f5d1d034596f6eb02173e531f86
Piyush Patel <piyushpatel2005@gmail.com>
2020-10-25 10:00:47 -0400

    Bridge pattern for flexibility

---
+++ src/main/java/bridge/Printer.java
@@ -0,0 +1,15 @@
+package bridge;
+
+import java.util.List;
+
+public abstract class Printer {
+
+    // doesn't know anything about the formatter being passed in here
+    public String print(Formatter formatter) {
+        return formatter.format(getHeader(), getDetails());
+    }
+
+    abstract protected List<Detail> getDetails();
+
+    abstract protected String getHeader();
+}
---
+++ src/main/java/bridge/shape1/Circle.java
@@ -0,0 +1,5 @@
+package bridge.shape1;
+
+// inheritance complicates the modifications
+public abstract class Circle extends Shape {
+}
