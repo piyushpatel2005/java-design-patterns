0bef479c3b20541f6480eaa51943132f7bd8b973
Piyush Patel <piyushpatel2005@gmail.com>
2020-10-25 01:04:10 -0400

    Adapter pattern

---
+++ src/main/java/adapter/AdapterSimpleDemo.java
@@ -0,0 +1,17 @@
+package adapter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AdapterSimpleDemo {
+
+    public static void main(String[] args) {
+        Integer[] arrayOfInts = new Integer[] { 42, 43, 44 };
+
+        List<Integer> listOfInts = Arrays.asList(arrayOfInts);
+
+        System.out.println(arrayOfInts);
+
+        System.out.println(listOfInts);
+    }
+}
