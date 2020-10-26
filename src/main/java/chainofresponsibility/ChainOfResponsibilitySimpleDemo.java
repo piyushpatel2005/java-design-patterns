b6a6c92bc19d1eae677a43ffb5430e34395a6a46
Piyush Patel <piyushpatel2005@gmail.com>
2020-10-25 23:01:13 -0400

    Chain of Responsibility

---
+++ src/main/java/chainofresponsibility/ChainOfResponsibilitySimpleDemo.java
@@ -0,0 +1,26 @@
+package chainofresponsibility;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ChainOfResponsibilitySimpleDemo {
+
+    private static final Logger logger = Logger.getLogger(ChainOfResponsibilitySimpleDemo.class.getName());
+
+    public static void main(String[] args) {
+
+        // level to log at
+        logger.setLevel(Level.FINER);
+
+        ConsoleHandler handler = new ConsoleHandler();
+
+        // level to publish at
+        handler.setLevel(Level.FINER);
+        logger.addHandler(handler);
+
+        logger.finest("Finest level of logging"); // won't appear
+        logger.finer("Finer level of loggin");
+        logger.fine("Fine level");
+    }
+}
