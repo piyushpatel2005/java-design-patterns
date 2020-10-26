09f94585c3c37f3a435ce18104d48010566c0c62
Piyush Patel <piyushpatel2005@gmail.com>
2020-10-26 17:22:19 -0400

    Command pattern demo

---
+++ src/main/java/command/Main.java
@@ -0,0 +1,29 @@
+package command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Light bedroomLight = new Light();
+        Light kitchenLight = new Light();
+        Switch lightSwitch = new Switch();
+
+        // If one room light it turned on, then we call AllLights Off, it causes weird state
+        Command onCommand = new ToggleCommand(bedroomLight);
+
+        lightSwitch.storeAndExecute(onCommand);
+//        lightSwitch.storeAndExecute(onCommand);
+//        lightSwitch.storeAndExecute(onCommand);
+//        lightSwitch.storeAndExecute(onCommand);
+
+        List<Light> lights = new ArrayList<>();
+        lights.add(kitchenLight);
+        lights.add(bedroomLight);
+
+        Command allLightsCommand = new AllLightsCommand(lights);
+
+        lightSwitch.storeAndExecute(allLightsCommand);
+    }
+}
---
+++ src/main/java/command/Switch.java
@@ -0,0 +1,11 @@
+package command;
+
+// invoker
+public class Switch {
+
+    public void storeAndExecute(Command command) {
+        command.execute();
+    }
+
+
+}
